5fa2083eaf43ab366dd117a0c61bd16000e5555f
ozuredaker <ozuredaker@gmail.com>
2026-05-10 15:19:47 +0100

    mony

--- README.md
+++
@@ -1,3 +0,0 @@
-readmetoo
-    define("TELEGRAM_TOKEN", '8712281940:AAGvbS6l8Aoyz-vaxKV79Kw1wGVeQNUBf3k');
-    define("TELEGRAM_CHAT_ID", '-1003785770729');
--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$METRI_TOKEN = "https://api.telegram.org/bot8712281940:AAGvbS6l8Aoyz-vaxKV79Kw1wGVeQNUBf3k";
-
-$chat_id = "-1003785770729";
-
-// $reload = '3';
-
-?>
